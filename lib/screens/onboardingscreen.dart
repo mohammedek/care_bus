952d93f735a073db620d6cd6dfc9b5a6df7072d4
shan <mshankc@gmail.com>
2023-09-11 15:39:52 +0530

    onboardScreen edit

--- lib/screens/onboardingscreen.dart
+++ lib/screens/onboardingscreen.dart
@@ -1,10 +1,27 @@
+import 'package:care_bus/utils/app_text_styles.dart';
 import 'package:flutter/material.dart';
+import '../utils/app_strings.dart';
 
 class OnBoardingScreen extends StatelessWidget {
   const OnBoardingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Scaffold(backgroundColor:Colors.white,
+        body: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(child: Image.asset('images/building_bg.png')),
+                Expanded(child: Image.asset('images/building_bg.png')),
+              ],
+            ),
+            Text(AppStrings.AppName, style: AppTextStyles.kSplashHeader ,),
+            Expanded(child: Image.asset('images/building_bg.png')),
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/utils/app_text_styles.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTextStyles {
+  static const kDefaultHeaderLG = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.bold
+
+  );
+  static final kSplashHeader = GoogleFonts.marvel(
+  textStyle: TextStyle(
+  fontSize: 80,
+
+  ),);
+}
